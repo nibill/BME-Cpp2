a0d002b4ef8701a3848c097279d75e0d158e9bc6
nibill <tet@tes.ch>
2019-10-13 19:09:31 +0200

    udpate

---
+++ EX02/main.cpp
@@ -0,0 +1,34 @@
+/* Exercise 02 - ""
+ *
+ * @author Thomas Buchegger
+ * @email thomas.buchegger@students.unibe.ch
+ * @created  08.10.2019 09:08
+ *
+ * University of Bern / Bern University of Applied Sciences
+ * Course 394661-FS2019-0: C++ Programming I
+ */
+
+#include <iostream>
+#include "ex2.h"
+#include "ex3.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "ex 2a:" << endl;
+    cout << "--------------------------------------" << endl;
+    void ex2a();
+    cout << endl;
+
+    cout << "ex 2b:" << endl;
+    cout << "--------------------------------------" << endl;
+    void ex2b();
+    cout << endl;
+
+    cout << "ex 3:" << endl;
+    cout << "--------------------------------------" << endl;
+    ex3();
+    cout << endl;
+    return 0;
+}
