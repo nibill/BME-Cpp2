68e34168b2d8b0fc3fbe1b5f9bbedc04531261ef
nibill <tet@tes.ch>
2019-11-03 19:19:46 +0100

    ex4 updated

--- EX04/ex4.cpp
+++ EX04/ex4.cpp
@@ -10,10 +10,14 @@ void ex4::ex4_3()
     LogToFile(log,1);
     vector <thread> myThreads;
     myThreads.clear();
-    for (unsigned int i=0 ; i < 10*thread::hardware_concurrency();i++){
+    for (unsigned int i=0 ; i < 10*thread::hardware_concurrency();i++)
+    {
         myThreads.push_back(thread(LogToFile,std::ref(log),i));
-     }
-    for (auto &it :myThreads){ it.join();}
+    }
+    for (auto &it :myThreads)
+    {
+        it.join();
+    }
 }
 
 void ex4::ex4_4()
@@ -21,13 +25,22 @@ void ex4::ex4_4()
     ThreadLogFileSafe logEx4_4;
     vector <thread> myThreads;
     myThreads.clear();
-    for (unsigned int i=0 ; i < 10*thread::hardware_concurrency();i++){
+    for (unsigned int i=0 ; i < 10*thread::hardware_concurrency();i++)
+    {
         myThreads.push_back(thread(LogToFileSafe,std::ref(logEx4_4),i));
-     }
-    for (auto &it :myThreads){ it.join();}
+    }
+    for (auto &it :myThreads)
+    {
+        it.join();
+    }
 }
 
 void ex4::LogToFile(ThreadLogFile &log, int value)
+{
+    log.print(std::this_thread::get_id(),value);
+}
+
+void ex4::LogToFileSafe(ThreadLogFileSafe &log ,int value)
 {
     log.print(std::this_thread::get_id(),value);
 }
